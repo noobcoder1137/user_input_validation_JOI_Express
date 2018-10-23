407e379faca509106c0794659ef0a90ac95ea1ec
Pedro Mercado <noobcoder1137@gmail.com>
2018-10-22 22:51:11 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const Joi = require('joi');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use('/public',express.static(path.join(__dirname,'static')));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'static','index.html'));
+});
+
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    const schema = Joi.object().keys({
+        email : Joi.string().trim().email().required(),
+        password: Joi.string().min(5).max(10).required()
+    });
+    Joi.validate(req.body,schema,(err,result)=>{
+        if(err){
+            console.log(err);
+            res.send('an error has occurred');
+        }
+        else{
+            console.log(result);
+            res.send('successfully posted data');
+        }
+        
+    })
+    
+});
+
+
+app.listen(3000);
